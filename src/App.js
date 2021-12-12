2f8cd7a478c7cedd0e7b1dc8603f2eb24f3f2dd4
demarchiworking <antonio.demarchi@g.globo>
2021-12-11 21:43:28 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import Axios from "axios";
+import Card from './components/card';
 
 function App() {
+  const [valor, setValores] = useState();
+  const[listResponse, setListResponse] = useState();
+  console.log(valor);
+  console.log(listResponse);
+
+  const alteracaoValores = (valor) =>{
+    setValores((valorAnterior)=>({
+      ...valorAnterior,
+      [valor.target.name]: valor.target.value
+    }))
+  };
+
+  const botaoClick = () => {
+    Axios.post("http://localhost:3001/registrar",{
+      nome: valor.nome,
+      valor: valor.valor,
+      categoria: valor.categoria,
+    }).then((response)=>{
+      console.log(response)
+    }).then((response) => {
+      console.log(response);
+    });
+};
+
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/getCards").then((response => {
+      setListResponse(response.data);
+    }));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app--container">
+      <div className="cadastro-container">
+      <h1>Shop</h1>
+      <input type="text" name="nome" placeholder="register" 
+      className="cadastro-input" onChange={alteracaoValores}/>
+      <input type="text" name="valor" placeholder="Preço"
+       className="cadastro-input" onChange={alteracaoValores}/>
+      <input type="text" name="categoria" placeholder="categoria"
+       className="cadastro-input" onChange={alteracaoValores}/>
+      
+      <button className="cadastro-butao" 
+      onClick={()=> botaoClick()}>Cadastrar</button>  
+      </div> 
+         
+    {typeof listResponse !== "undefined" && 
+    listResponse.map((valor => {
+      return <Card></Card>;
+    }))}
     </div>
   );
 }
